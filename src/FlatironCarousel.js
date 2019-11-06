1fa4c2a057dcc53c36f7fcb35792e69b7d0645a0
github username <github email address>
2019-11-06 16:21:40 -0500

    fixed images, they are now showing in carousel.

--- src/FlatironCarousel.js
+++ src/FlatironCarousel.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Image } from 'semantic-ui-react'
 import { Carousel } from 'react-responsive-carousel';
 
 export default class FlatironCarousel extends React.Component {
@@ -17,30 +16,28 @@ export default class FlatironCarousel extends React.Component {
     render() {
         return (
             <div>
-            
-            <Carousel autoPlay={true} swipeable={true} showThumbs={false} infiniteLoop={true} interval={2500}>
-                <div>
-                <Image as='a' href="https://flatironschool.com/" src="http://pngimg.com/uploads/football/football_PNG52759.png" />
-                    {/* <img src="EwzujL1Imgur.jpg" alt='flatiron people'/> */}
-                    {/* <p className="legend">Legend 1</p> */}
-                </div>
-                <div>
-                    <Image src="" alt='flatiron people'/>
-                    {/* <p className="legend">Legend 2</p> */}
-                </div>
-                <div>
-                    <img src="src/images/Image from iOS (6).jpg" alt='flatiron people'/>
-                    {/* <p className="legend">Legend 3</p> */}
-                </div>
-                <div>
-                    <img src="src/images/Image from iOS (7).jpg" alt='flatiron people'/>
-                    {/* <p className="legend">Legend 3</p> */}
-                </div>
-                <div>
-                    <img src="src/images/Image from iOS.jpg" alt='flatiron people'/>
-                    {/* <p className="legend">Legend 3</p> */}
-                </div>
-            </Carousel>
+                <Carousel autoPlay={true} swipeable={true} showThumbs={false} infiniteLoop={true} interval={2500}>
+                    <div>
+                    <img src="https://i.ibb.co/NKY8R5B/Image-from-i-OS-6.png" alt="flatiron people" border="0"/>
+                        
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/GkkTN9h/Image-from-i-OS-7.png" alt='flatiron people'/>
+                        
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/nCWT35c/Image-from-i-OS-5.png" alt='flatiron people'/>
+                        
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/R651XBm/Image-from-i-OS.png" alt='flatiron people'/>
+                    
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/5FXS1bf/Ewzuj-L1-Imgur.png" alt='flatiron people'/>
+                        
+                    </div>
+                </Carousel>
             </div>
         );
     }
